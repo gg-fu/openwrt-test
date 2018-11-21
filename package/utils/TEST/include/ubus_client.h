ec4751a3914199a1cd8bf686493e0b6701f9086e
gg-fu <13432820164@163.com>
2018-11-21 18:50:13 +0800

    刪除TEST

--- package/utils/TEST/include/ubus_client.h
+++
@@ -1,6 +0,0 @@
-#ifndef _UBUS_CLIENT_H
-#define _UBUS_CLIENT_H
-
-int Add(int a,int b);
-
-#endif
--- package/utils/TEST/src/ubus_client.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int Add(int a,int b)
-{
-	return (a+b);
-}
